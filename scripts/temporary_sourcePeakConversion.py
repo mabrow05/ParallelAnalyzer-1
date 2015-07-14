de0bb63dccbb75cac3f663dd650c66a814ae5b84
Michael Brown <michael.brown1@uky.edu>
2015-07-13 21:08:08 -0400

    Calculated new linearity curves and residuals using new smeared EQ values

---
+++ scripts/temporary_sourcePeakConversion.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import os
+
+runPeriods = [1,2,3,4,5,6,7,8,9,10,11,12]
+InEnergy = False;
+filename = ""
+filein = None
+
+for runPeriod in runPeriods:
+    input_hold = []
+    if InEnergy:
+        filename = "../residuals/source_runs_EnergyPeaks_RunPeriod_%i_hold.dat"%runPeriod
+        filein = open(filename, "r")
+        for line in filein:
+            input_hold.append(line.split())
+        filein.close()
+    else:
+        filename = "../residuals/source_runs_RunPeriod_%i_hold.dat"%runPeriod
+        filein = open(filename, "r")
+        for line in filein:
+            input_hold.append(line.split())
+        filein.close()
+    
+    for line in input_hold:
+        print '%s %s %s %s %s %s %s %s %s %s'%(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9])
+
+    fileout = open("../residuals/source_runs_RunPeriod_%i.dat"%runPeriod, "w")
+    eQ = 0.
+    for line in input_hold:
+        #print line[1]
+        if float(line[1])==98.2:
+            eQ=80.5
+        elif float(line[1])==331.2:
+            eQ=317.8
+        elif float(line[1])==443.0:
+            eQ=448.8
+        elif float(line[1])==928.0:
+            eQ=926.0
+        fileout.write('%s %s %s %s %s %s %s %s %s %s\n'%(line[0], eQ, line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9]))
+
+    fileout.close()
