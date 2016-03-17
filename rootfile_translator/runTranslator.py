a7405573dda1332bf32bf6292fb73440e0404dee
Michael Brown <michael.brown1@uky.edu>
2016-03-17 12:20:53 -0400

    Small edits

--- rootfile_translator/runTranslator.py
+++ rootfile_translator/runTranslator.py
@@ -1,6 +1,6 @@
 import os
 
-for run in range(17125, 17198,1):
+for run in range(17125, 19966,1):
     filename = os.getenv("REPLAY_PASS4")+"replay_pass4_%i.root"%run
     if os.path.isfile(filename):
         os.system("./rootfile_translator %i"%run)
