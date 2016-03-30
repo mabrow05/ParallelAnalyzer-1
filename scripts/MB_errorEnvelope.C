52dc962977f9c02acd14216aa503d62eb968f3ae
Michael Brown <michael.brown1@uky.edu>
2016-03-30 11:02:18 -0400

    Checking error envelope after new trigger functions

--- scripts/MB_errorEnvelope.C
+++ scripts/MB_errorEnvelope.C
@@ -55,7 +55,7 @@ void MB_errorEnvelope(Int_t calLow, Int_t calHigh, Int_t pmt, bool postReplayPas
   else if (calPeriodLow==calPeriodHigh && PMT) sprintf(tempEast,"../residuals/residuals_East_runPeriod_%i_PMTE%i.dat", calPeriodLow,PMT);
   ifstream fileEast(tempEast);
 
-  const size_t N = 500;
+  const size_t N = 1000;
   TString sourceEast[N];
   Int_t runEast[N];
   Double_t resEast[N];
