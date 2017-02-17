a2d9e671f5be237b172f86fc28f9dbed4a623db5
Michael Brown <michael.brown1@uky.edu>
2017-02-17 02:25:31 -0500

    Doing these effing position response things

--- include/MWPCPositionResponse.cc
+++ include/MWPCPositionResponse.cc
@@ -5,13 +5,6 @@
 MWPCCathodeHandler::MWPCCathodeHandler() {
 
   posEX = posEY = posWX = posWY = 0.;
-  nClippedEX = nClippedEY = nClippedWX = nClippedWY = 0;
-  goodWiresEX = goodWiresEY = goodWiresWX = goodWiresWY = {0.};
-  clippedEX.resize(16,0);
-  clippedEY.resize(16,0);
-  clippedWX.resize(16,0);
-  clippedWY.resize(16,0);
-  boolClipped = false;
   boolPedSubtr = false;
 
 };
@@ -31,17 +24,6 @@ void MWPCCathodeHandler::setPedestalValues(double *ex,double *ey,double *wx,doub
 };
 
 
-void MWPCCathodeHandler::doClipping() {
-
-  for ( int i=0; i<16; ++i ) {
-    if ( cathEX[i]>4095. ) { clippedEX[i]=1; nClippedEX++; }
-    if ( cathEY[i]>4095. ) { clippedEY[i]=1; nClippedEY++; }
-    if ( cathWX[i]>4095. ) { clippedWX[i]=1; nClippedWX++; }
-    if ( cathWY[i]>4095. ) { clippedWY[i]=1; nClippedWY++; }
- }
-  boolClipped = true;
-};
-
 void MWPCCathodeHandler::doPedestalSubtraction() {
 
   for ( int i=0; i<16; i++ ) {   
@@ -54,78 +36,208 @@ void MWPCCathodeHandler::doPedestalSubtraction() {
 };
 
 
+std::vector<int> MWPCCathodeHandler::doClipping(std::vector<int> wires, double* signal) {
 
-void MWPCCathodeHandler::fitCathResponse() {
+  std::vector <int> clipped;
+  for ( unsigned int i=0; i<wires.size(); ++i ) {
+    if ( signal[wires[i]] > 4090. ) clipped.push_back(wires[i]); 
+  }
+  return clipped;
+};
 
-  if ( !boolClipped )  doClipping();
+void MWPCCathodeHandler::doThresholdCheck() {
+  
+  for ( int i=0; i<16; i++ ) {   
+    if ( pedSubtrEX[i] > cathodeThreshold ) signalEX.push_back(i);
+    if ( pedSubtrEY[i] > cathodeThreshold ) signalEY.push_back(i);
+    if ( pedSubtrWX[i] > cathodeThreshold ) signalWX.push_back(i);
+    if ( pedSubtrWY[i] > cathodeThreshold ) signalWY.push_back(i);
+  }
+  
+};
+
+
+
+void MWPCCathodeHandler::findAllPositions() {
+
   if ( !boolPedSubtr ) doPedestalSubtraction();
+  if ( !boolThresholdCheck ) doThresholdCheck();
+
+  //East X
+  clippedEX = doClipping(signalEX, cathEX);
+  posEX = fitCathResponse(signalEX, clippedEX, pedSubtrEX,wireposEX);
   
-  if ( nClippedEX > 2 ) /////////////////////////////////////
-
-  chooseWires(pedSubtrEX, clippedEX);
-  chooseWires(pedSubtrEY, clippedEY);
-  chooseWires(pedSubtrWX, clippedWX);
-  chooseWires(pedSubtrWY, clippedWY);
-
-
-  posEX = fitGaus(goodWiresEX);
-  posEY = fitGaus(goodWiresEY);
+  //East Y
+  clippedEY = doClipping(signalEY, cathEY);
+  posEY = fitCathResponse(signalEY, clippedEY, pedSubtrEY, wireposEY);
   
-  posWX = fitGaus(goodWiresWX);
-  posWY = fitGaus(goodWiresWY);
+  //West X
+  clippedWX = doClipping(signalWX, cathWX);
+  posWX = fitCathResponse(signalWX, clippedWX, pedSubtrWX,wireposWX);
+  
+  //West Y
+  clippedWY = doClipping(signalWY, cathWY);
+  posWY = fitCathResponse(signalWY, clippedWY, pedSubtrWY,wireposWY);
 
 };
 
-std::vector <int> MWPCCathodeHandler::chooseWires(double *cath, std::vector <int> clipped) {
-  
-  int maxWire = 100;
-  double maxVal = cathodeThreshold;
-  int wiresAboveThresh = 0;
-
-  std::vector <int> wires;
-
-  // Choose max wire
-  for ( int i=0; i<16; ++i ) {
-    if ( cath[i] > cathodeThreshold ) wiresAboveThresh++;
-
-    if ( cath[i] > maxVal && !clipped[i] ) {
-      maxWire = i;
-      maxVal = cath[i];
+int getMaxWire(std::vector <int> wires, double *sig) {
+  int maxWire = 0;
+  double max = 0.;
+  for ( unsigned int i=0; i<wires.size(); ++i ) {
+    if ( sig[wires[i]]>max ) {
+      max = sig[wires[i]];
+      maxWire = wires[i];
     }
   }
-
-  
-  
-  if ( maxWire!=100 && maxWire!=0 && maxWire!=15 ) { // So there is a max wire not on the edge
-                                                     // and it is not a clipped wire   
-    int lowWire = -100;
-    int highWire = 100;
-    
-    for ( int i = maxWire; i>-1; --i )
-      if ( !clipped[i] ) { lowWire == i; break; }
-    
-    for ( int i = maxWire; i<16; ++i )
-      if ( !clipped[i] ) { highWire == i; break; }
-    
-    if ( lowWire!=-100 ) wires.push_back(lowWire);
-    wires.push_back(maxWire);
-    if ( highWire!=100 ) wires.push_back(highWire);
-    
-    return wires; 
-  }
-  
-  else if ( maxWire==0 ) {
-
-    int lowWire = -1;
-    int highWire = 100;
-
-  }
-
-    
-    
-      
+  return maxWire;
 };
 
+int MWPCCathodeHandler::getMaxWireNotClipped(std::vector <int> wires, std::vector <int> clipped, double *sig) {
+  
+  int maxWire = 0;
+  double max = 0.;
+  // Choose max wire
+  for ( unsigned int i=0; i<wires.size(); ++i ) {
+    for ( unsigned int j=0; j<clipped.size(); ++j ) {
+      if ( sig[wires[i]]>max && wires[i]!=clipped[j] ) {
+	max = sig[wires[i]];
+	maxWire = wires[i];
+      }
+    }
+  }
+  return maxWire;
+};
+  
+
+double MWPCCathodeHandler::fitCathResponse(std::vector <int> wires, std::vector<int> clip, double *sig, double *pos) {
+
+  std::vector <double> posFit(3,0.);
+  std::vector <double> valFit(3,0.);
+  double posInc = 10.16;
+
+  unsigned int numWires = wires.size();
+  unsigned int nClipped = clip.size();
+  
+  if ( numWires==0 ) return 0.;
+  else if ( numWires==1 ) return pos[wires[0]];
+  else if ( numWires==2 ) {
+    
+    if ( nClipped==2 ) return ( pos[wires[0]] + pos[wires[1]] ) / 2.;
+    
+    int max = getMaxWire(wires, sig);
+    
+    if ( max==0 ) { // Handle edge wires
+      valFit[0] = cathodeThreshold; posFit[0] = pos[0]<0. ? (pos[0] - posInc) : (pos[0] + posInc) ;
+      valFit[1] = sig[wires[0]]; posFit[1] = pos[wires[0]];
+      valFit[2] = sig[wires[1]]; posFit[2] = pos[wires[1]];
+    }
+    else if ( max==15 ) {
+      valFit[0] = cathodeThreshold; posFit[0] = pos[15]<0. ? (pos[15] - posInc) : (pos[15] + posInc) ;
+      valFit[1] = sig[wires[0]]; posFit[1] = pos[wires[0]];
+      valFit[2] = sig[wires[1]]; posFit[2] = pos[wires[1]];
+    }
+    else { // pick one more wire 
+
+      if ( wires[0]==max ) { posFit[0] = pos[wires[0]-1]; valFit[0] = cathodeThreshold; }
+      else { posFit[0] = pos[wires[1]+1]; valFit[0] = cathodeThreshold; }
+
+      valFit[1] = sig[wires[0]]; posFit[1] = pos[wires[0]];
+      valFit[2] = sig[wires[1]]; posFit[2] = pos[wires[1]];
+    }
+    return fitGaus(posFit, valFit);
+  }
+
+  else { // At least 3 wires above threshold
+
+    int max = getMaxWire(wires, sig);
+    
+    if ( nClipped < 2 ) { // Less than 2 clipped, use max wire, and maxWire-1 and +1
+      if ( max==0 ) {
+	valFit[0] = sig[wires[0]]; posFit[0] = pos[wires[0]];
+	valFit[1] = sig[wires[1]]; posFit[1] = pos[wires[1]];
+	valFit[2] = sig[wires[2]]; posFit[2] = pos[wires[2]];
+      }
+      else if ( max==15 ) {
+	valFit[0] = sig[wires[numWires-3]]; posFit[0] = pos[wires[numWires-3]];
+	valFit[1] = sig[wires[numWires-2]]; posFit[1] = pos[wires[numWires-2]];
+	valFit[2] = sig[wires[numWires-1]]; posFit[2] = pos[wires[numWires-1]];
+      }
+      else {
+	valFit[0] = sig[max-1]; posFit[0] = pos[max-1];
+	valFit[1] = sig[max]; posFit[1] = pos[max];
+	valFit[2] = sig[max+1]; posFit[2] = pos[max+1];
+      }
+      return fitGaus(posFit, valFit);
+    }
+
+    else if ( nClipped==2 ) { // 2 clipped and at least 1 other signal 
+
+      if ( numWires>3 ) { // 2 or more other wires over threshold
+
+	if (clip[0]==0) {
+	  valFit[0] = sig[wires[2]]; posFit[0] = pos[wires[2]];
+	  valFit[1] = 4096.*1.25; posFit[1] = (pos[clip[0]] + pos[clip[1]])/2.;
+	  valFit[2] = sig[wires[3]]; posFit[2] = pos[wires[3]];
+	}
+	else if (clip[1]==15) {
+	  valFit[0] = sig[wires[numWires-4]]; posFit[0] = pos[wires[numWires-4]];
+	  valFit[1] = 4096.*1.25; posFit[1] = (pos[clip[0]] + pos[clip[1]])/2.;
+	  valFit[2] = sig[wires[numWires-3]]; posFit[2] = pos[wires[numWires-3]];
+	}
+	else {
+	  valFit[0] = sig[clip[0]-1]; posFit[0] = pos[clip[0]-1];
+	  valFit[1] = 4096.*1.25; posFit[1] = (pos[clip[0]] + pos[clip[1]])/2.;
+	  valFit[2] = sig[clip[0]+2]; posFit[2] = pos[clip[0]+2];
+	}
+	return fitGaus(posFit, valFit);
+      }
+
+      else { // 1 other wire over threshold
+	
+	if ( clip[0]==wires[0] ) {
+	  valFit[0] = sig[wires[2]]; posFit[0] = pos[wires[2]];
+	  valFit[1] = 4096. + sig[wires[2]]; posFit[1] = pos[clip[1]];
+	  valFit[2] = 4096.;  posFit[2] = pos[clip[0]];
+	}
+	else {
+	  valFit[0] = sig[wires[0]]; posFit[0] = pos[wires[0]];
+	  valFit[1] = 4096. + sig[wires[0]]; posFit[1] = pos[clip[0]];
+	  valFit[2] = 4096.;  posFit[2] = pos[clip[1]];
+	}
+	return fitGaus(posFit, valFit);
+      }
+    }
+      
+    else {
+      if ( (numWires - nClipped) > 2 ) { // Check that there are at least 3 points that aren't overflow. Use average of
+                              // clipped*nClipped and the other wires above thresh
+	double ave = 0.;
+	for ( unsigned int i=0; i<nClipped; ++i ) {
+	  ave += pos[clip[i]];
+	}
+	posFit[0] = ave/nClipped; valFit[0] = 4096.*nClipped;
+	posFit[1] = pos[wires[0]]; valFit[1] = sig[wires[0]];
+	posFit[2] = pos[wires[1]]; valFit[2] = sig[wires[1]];
+	for ( unsigned int i=2; i<(numWires-nClipped); ++i ) {
+	  posFit.push_back(pos[wires[i]]); valFit.push_back(sig[wires[i]]);
+	}
+	return fitGaus(posFit, valFit);
+      }
+    
+    
+      else { // Else you just take the average and call it a day
+	double ave = 0.;
+	for ( unsigned int i=0; i<nClipped; ++i ) {
+	  ave += pos[clip[i]];
+	}
+	return ave/nClipped;
+      }
+    }
+  }
+};
+  
+  
 void MWPCCathodeHandler::fitGaus(double *wires) {
   //make sure all three points are positive
 };
--- include/MWPCPositionResponse.hh
+++ include/MWPCPositionResponse.hh
@@ -14,18 +14,19 @@ public:
   
   void setCathResponse(double *ex,double *ey,double *wx,double *wy);
   void setPedestalValues(double *ex,double *ey,double *wx,double *wy);
-  void doClipping();
-  void fitCathResponse();
+  void findAllPositions();
+  
+  
   
   double getPosEX() { return posEX; }
   double getPosEY() { return posEY; }
   double getPosWX() { return posWX; }
   double getPosWY() { return posWY; }
 
-  int getnClippedEX() { return nClippedEX; }
-  int getnClippedEY() { return nClippedEY; }
-  int getnClippedWX() { return nClippedWX; }
-  int getnClippedWY() { return nClippedWY; }
+  int getnClippedEX() { return clippedEX.size(); }
+  int getnClippedEY() { return clippedEY.size(); }
+  int getnClippedWX() { return clippedWX.size(); }
+  int getnClippedWY() { return clippedWY.size(); }
   
 
 
@@ -35,27 +36,36 @@ private:
   double *cathEX, *cathEY, *cathWX, *cathWY;
   double *pedEX, *pedEY, *pedWX, *pedWY;
   double pedSubtrEX[16],pedSubtrEY[16],pedSubtrWX[16],pedSubtrWY[16];
-  double posEX, posEY, posWX, posWY;
-  int nClippedEX, nClippedEY, nClippedWX, nClippedWY;
-  std::vector <int> clippedEX, clippedEY, clippedWX, clippedWY;
+  double posEX, posEY, posWX, posWY; // Final position of the event
+  //int nClippedEX, nClippedEY, nClippedWX, nClippedWY;
+  std::vector <int> clippedEX, clippedEY, clippedWX, clippedWY; // Vector of clipped wires
+  std::vector <int> signalEX, signalEY, signalWX, signalWY; // Holds wires with signal above threshold
+  std::vector <int> fitWiresEX, fitWiresEY, fitWiresWX, fitWiresWY; // The wires to be fitted
 
-  std::vector <int> goodWiresEX,goodWiresEY,goodWiresWX,goodWiresWY;
-
+  void doThresholdCheck(); // Sets all of the wires that were above threshold in signal**
+  
   std::vector <int> chooseWires(double *cath, std::vector <int> clipped);
+  std::vector <int>  doClipping(std::vector<int> wires, double *sig); //Checks for clipped wires
+  double fitCathResponse(std::vector <int> wires, std::vector<int> clip, double *sig, double *pos); // Returns the position of the cathode signal in a plane
+  int getMaxWire(std::vector <int> wires, double *sig); // returns max wire
 
-  bool boolClipped;  // Whether or not the clipping has been calculated
+  
   bool boolPedSubtr; // Whether or not the pedestal subtraction has been calculated
-
+  bool boolThresholdCheck; // Whether or not the threshold has been checked has been calculated
+  
   const double cathodeThreshold = 100.;
   const double wireposEX[16] = {76.20, 66.04, 55.88, 45.72, 35.56, 25.40, 15.24, 5.08, 
-		    -5.08, -15.24, -25.40, -35.56, -45.72, -55.88, -66.04, -76.20};
+				-5.08, -15.24, -25.40, -35.56, -45.72, -55.88, -66.04, -76.20};
   const double wireposEY[16] = {76.20, 66.04, 55.88, 45.72, 35.56, 25.40, 15.24, 5.08, 
-		    -5.08, -15.24, -25.40, -35.56, -45.72, -55.88, -66.04, -76.20};
+				-5.08, -15.24, -25.40, -35.56, -45.72, -55.88, -66.04, -76.20};
   const double wireposWY[16] = {76.20, 66.04, 55.88, 45.72, 35.56, 25.40, 15.24, 5.08, 
-		    -5.08, -15.24, -25.40, -35.56, -45.72, -55.88, -66.04, -76.20};
+				-5.08, -15.24, -25.40, -35.56, -45.72, -55.88, -66.04, -76.20};
   const double wireposWX[16] = {-76.20, -66.04, -55.88, -45.72, -35.56, -25.40, -15.24, -5.08,
-		    5.08, 15.24, 25.40, 35.56, 45.72, 55.88, 66.04, 76.20};
+				5.08, 15.24, 25.40, 35.56, 45.72, 55.88, 66.04, 76.20};
 }
+
 
 #endif
 
+
+  
