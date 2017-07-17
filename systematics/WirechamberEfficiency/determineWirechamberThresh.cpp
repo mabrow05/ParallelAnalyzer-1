323371a0475fcc332a193e39b630518ab87eab11
Michael Brown <michael.brown1@uky.edu>
2017-07-17 17:20:57 -0400

    Added this sweet code

---
+++ systematics/WirechamberEfficiency/determineWirechamberThresh.cpp
@@ -0,0 +1,76 @@
+#include <TChain.h>
+#include <TH1D.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TMath.h>
+
+#include <vector>
+#include <iostream>
+
+int main() {
+
+  Double_t eastEff = 0.991;
+  Double_t westEff = 0.994;
+
+  std::vector <int> runs {19899,19900,19902,19904,19924,19925,19927,19929};
+
+  TChain *chain = new TChain("revCalSim");
+
+  for (auto run:runs) {
+    chain->Add(TString::Format("%s/sources/revCalSim_%i_Sn113.root",getenv("REVCALSIM"),run));
+  }
+  
+  Double_t eastThresh = 0.8;
+  Double_t westThresh = 0.8;  
+  Double_t eff = 1.;
+
+  Double_t trigg, total;
+
+  while (eastEff<eff) {
+    eastThresh+=0.1;
+    total = chain->GetEntries("side==0 && type==0 && PID==1");
+    trigg = chain->GetEntries(TString::Format("side==0 && type==0 && PID==1 && MWPCEnergyE>%f",eastThresh));
+    eff = trigg/total;   
+  }
+  eastThresh-=0.1;  
+  while (eastEff<eff) {
+    eastThresh+=0.01;
+    total = chain->GetEntries("side==0 && type==0 && PID==1");
+    trigg = chain->GetEntries(TString::Format("side==0 && type==0 && PID==1 && MWPCEnergyE>%f",eastThresh));
+    eff = trigg/total;  
+  }
+  eastThresh-=0.01;
+  while (eastEff<eff) {
+    eastThresh+=0.001;
+    total = chain->GetEntries("side==0 && type==0 && PID==1");
+    trigg = chain->GetEntries(TString::Format("side==0 && type==0 && PID==1 && MWPCEnergyE>%f",eastThresh));
+    eff = trigg/total;  
+  }
+
+  //////////////////WEST///////////////////////
+  eff=1.;
+
+  while (westEff<eff) {
+    westThresh+=0.1;
+    total = chain->GetEntries("side==0 && type==0 && PID==1");
+    trigg = chain->GetEntries(TString::Format("side==0 && type==0 && PID==1 && MWPCEnergyE>%f",westThresh));
+    eff = trigg/total;   
+  }
+  westThresh-=0.1;  
+  while (westEff<eff) {
+    westThresh+=0.01;
+    total = chain->GetEntries("side==0 && type==0 && PID==1");
+    trigg = chain->GetEntries(TString::Format("side==0 && type==0 && PID==1 && MWPCEnergyE>%f",westThresh));
+    eff = trigg/total;  
+  }
+  westThresh-=0.01;
+  while (westEff<eff) {
+    westThresh+=0.001;
+    total = chain->GetEntries("side==0 && type==0 && PID==1");
+    trigg = chain->GetEntries(TString::Format("side==0 && type==0 && PID==1 && MWPCEnergyE>%f",westThresh));
+    eff = trigg/total;  
+  }
+
+  std::cout << "East Threshold = " << eastThresh << "\n";
+  std::cout << "West Threshold = " << westThresh << "\n";
+}
