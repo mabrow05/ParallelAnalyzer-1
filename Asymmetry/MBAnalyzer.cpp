652262feeb04fad808b37862315394d626d1404d
Michael Brown <michael.brown1@uky.edu>
2017-07-17 13:18:56 -0400

    nothing

--- Asymmetry/MBAnalyzer.cpp
+++ Asymmetry/MBAnalyzer.cpp
@@ -142,7 +142,7 @@ int main(int argc, char* argv[])
   // CLOCK TIMES.
   //****************************************************************
   //****************************************************************
-  bool UNBLIND = true;
+  bool UNBLIND = false;
 
 
   if (UNBLIND) {
